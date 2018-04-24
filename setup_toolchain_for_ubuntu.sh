501b1dc0f30dbd93876606e88e8a1c42d5b7caf0
何亚红 <2229388563@qq.com>
2018-04-24 11:20:10 +0800

    更新脚本

--- setup_toolchain_for_ubuntu.sh
+++ setup_toolchain_for_ubuntu.sh
@@ -2,7 +2,7 @@
 echo Installing sdcc
 apt-get install sdcc make  -y
 echo Installing cc-tool
-apt-get install libusb-1.0 libboost-all-dev gcc build-essential -y 
+apt-get install libusb-1.0 libboost-all-dev gcc g++ pkg-config build-essential -y 
 cd toolchain/cc-tool
 ./configure
 make && make install
