19910ccea9fc2dc21dbba2b5bfd261022fdafcfe
何亚红 <2229388563@qq.com>
2018-04-25 12:27:03 +0800

    添加gets()的用法示例

--- test_uart/test_uart.c
+++ test_uart/test_uart.c
@@ -4,8 +4,11 @@
 void main(void) {
 	Hal_UART_Init();
 	while(1) {
-		printf("Hello, World!\n\rInput:");
-		printf("\n\rnumber:%d\n\r",getchar());
+		char temp[32];
+		printf("Hello, World!\n\rInput Char:\n\r");
+		printf("number:%d\n\rInput string:\n\r",getchar());
+		gets(temp);//最多只能输入31字节
+		printf("string:%s\n\r",temp);
 	}
 }
 
